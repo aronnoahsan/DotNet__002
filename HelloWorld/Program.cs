d58031d00c0a7184db1daa71b6f89beeca2ab1b7
Aronno Ahsan <aronno.ahsan.sust@gmail.com>
2023-11-28 14:36:46 +0600

    Updated

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,8 +8,8 @@ namespace Application
 {
     public class Person
     {
-        private  string firstName;
-        private string lastName;
+        private  string? firstName;
+        private string? lastName;
         private int age;
         private bool isValidName( string? name)
         {
@@ -44,26 +44,10 @@ namespace Application
     {
         public static void Main(string[] args)
         {
-            int age = 18;
-            Console.WriteLine($"The age is {age}");
-            double height = 1.8, weight = 65, bmi;
-            bmi = weight / Math.Pow(height, 2);
-            Console.WriteLine($"The BMI is {bmi:0.###}");
-            bool result = 0.3 == 0.1 + 0.2;
-            Console.WriteLine(result);
-            bool result2 = "a" == "a";
-            Console.WriteLine(result2);
-            char c1 = 'a', c2 = 'b';
-            bool result3 = c1 < c2;
-            Console.WriteLine(result3);
-            string message = "Hello World";
-            Console.WriteLine(message);
-            Console.WriteLine(message.Length);
-            Console.WriteLine(message[4]);
             Console.WriteLine("Executing");
             Person p1 = new();
-            p1.SetFirstName("a");
-            p1.SetLastName("b");
+            p1.SetFirstName("Aronno");
+            p1.SetLastName("Ahsan");
             string name = p1.GetFullName();
             Console.WriteLine(name);
         }
