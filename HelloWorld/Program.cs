cd47bb8112a8b1da9e3c0888e52278f5dba043e6
Aronno Ahsan <aronno.ahsan.sust@gmail.com>
2023-11-28 10:35:44 +0600

    class executing

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,55 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.Intrinsics.X86;
+using System;
 
-int age = 18;
-Console.WriteLine($"The age is {age}");
-double height = 1.8, weight = 65, bmi;
-bmi = weight / Math.Pow(height, 2);
-Console.WriteLine($"The BMI is {bmi:0.###}");
-bool result = 0.3 == 0.1 + 0.2;
-Console.WriteLine(result);
-bool result2 = "a" == "a";
-Console.WriteLine(result2);
-char c1 = 'a', c2 = 'b';
-bool result3 = c1 < c2;
-Console.WriteLine(result3);
-string message = "Hello World";
-Console.WriteLine(message);
-Console.WriteLine(message.Length);
-Console.WriteLine(message[4]);
-class Person
+
+
+namespace Application
 {
-   private  string firstName;
-    private string lastName;
-    private int age;
-    private bool isValidName( string? name)
+    public class Person
     {
-        return !string.IsNullOrEmpty(name);
+        private  string firstName;
+        private string lastName;
+        private int age;
+        private bool isValidName( string? name)
+        {
+            return !string.IsNullOrEmpty(name);
+        }
+
+        public string GetFullName()
+        {
+            return $"{this.firstName} {this.lastName}";
+        }
+
+        public Person SetFirstName(string? firstName)
+        {
+            if (isValidName(firstName))
+            {
+                this.firstName = firstName;
+            }
+            return this;
+        }
+        public Person SetLastName(string? lastName)
+        {
+            if (isValidName(lastName))
+            {
+                this.lastName = lastName;
+            }
+            return this;
+        }
     }
 
-    public string GetFullName()
-    {
-        return $"{this.firstName} {this.lastName}";
-    }
 
-    public Person SetFirstName(string? firstName)
+    public class Execute
     {
-        if (isValidName(firstName))
+        public static void Main(string[] args)
         {
-            this.firstName = firstName;
+            int age = 18;
+            Console.WriteLine($"The age is {age}");
+            double height = 1.8, weight = 65, bmi;
+            bmi = weight / Math.Pow(height, 2);
+            Console.WriteLine($"The BMI is {bmi:0.###}");
+            bool result = 0.3 == 0.1 + 0.2;
+            Console.WriteLine(result);
+            bool result2 = "a" == "a";
+            Console.WriteLine(result2);
+            char c1 = 'a', c2 = 'b';
+            bool result3 = c1 < c2;
+            Console.WriteLine(result3);
+            string message = "Hello World";
+            Console.WriteLine(message);
+            Console.WriteLine(message.Length);
+            Console.WriteLine(message[4]);
+            Console.WriteLine("Executing");
+            Person p1 = new();
+            p1.SetFirstName("a");
+            p1.SetLastName("b");
+            string name = p1.GetFullName();
+            Console.WriteLine(name);
         }
-        return this;
-    }
-    public Person SetLastName(string? lastName)
-    {
-        if (isValidName(lastName))
-        {
-            this.lastName = lastName;
-        }
-        return this;
+
     }
 }
-//Person p1 = new Person();
-//Person p1 = new();
 
-Person p1 = new();
+
+
+
